fd912239c3ab7ebac2d7f46a99ca8cb76b56833a
Usagi Ito <usagi@WonderRabbitProject.net>
2014-04-17 15:56:32 +0900

    add entry sdl2 sdl1 glut

--- subsystem.hxx
+++ subsystem.hxx
@@ -8,4 +8,10 @@
   #include "glfw3.hxx"
 #elif defined(WRP_WONDERLAND_SUBSYSTEM_GLFW2)
   #include "glfw2.hxx"
+#elif defined(WRP_WONDERLAND_SUBSYSTEM_SDL2)
+  #include "sdl2.hxx"
+#elif defined(WRP_WONDERLAND_SUBSYSTEM_SDL1)
+  #include "sdl1.hxx"
+#elif defined(WRP_WONDERLAND_SUBSYSTEM_GLUT)
+  #include "glut.hxx"
 #endif
